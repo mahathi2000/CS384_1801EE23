5331a1904463085ab4fe28e5f3e1077e7fc68d15
mahathi2000 <mahathi.muddhey@gmail.com>
2020-11-29 18:15:24 +0530

    code for branch_stength.csv added as stated in step 1

---
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -0,0 +1,104 @@
+import os
+import re
+import csv
+import operator
+import shutil
+import pandas as pd
+import numpy as np
+import math
+import hashlib
+
+os.system("cls")
+
+def make_branch_strength(filename):
+    all_rolls=pd.read_csv(filename)
+    branch_code=[]
+    with open(filename,'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        next(reader)
+        for row in reader:
+            roll=row[0]
+            branch=roll[4:6]
+            branch_code.append(branch)
+        
+        all_rolls['BRANCH_CODE']=branch_code
+
+        #print(all_rolls)
+    df=all_rolls
+    new_df=df.groupby(['BRANCH_CODE']).size()
+    file_name='branch_strength.csv'
+    path=os.getcwd()
+    # directory = "groups"
+    # # Parent Directory path 
+    # parent_dir = os.getcwd()
+    
+    # # Path 
+    # path = os.path.join(parent_dir, directory)
+    # os.mkdir(path)
+
+
+
+    file_path=os.path.join(path,file_name)
+    new_df.to_csv(file_path,header=True,index=True)
+    df=pd.read_csv(file_name)
+    #adding header to the dataframe
+    df.columns = ['BRANCH_CODE','STRENGTH']
+    #sorting the dataframe wrt STRENGTH
+    new_df=df.sort_values(by=['STRENGTH'],ascending=False)
+    #converting the df to csv
+    new_df.to_csv(file_path,header=True,index=False)
+
+
+
+    pass 
+
+
+
+
+
+
+
+def group_allocation(filename, number_of_groups):
+    # Entire Logic 
+	# You can add more functions, but in the test case, we will only call the group_allocation() method,
+
+    # cwd=os.getcwd()
+    # path=cwd+'/groups'
+    # if os.path.exists(path):
+    #     shutil.rmtree(path)
+
+    # directory = "groups"
+    # # Parent Directory path 
+    # parent_dir = os.getcwd()
+    
+    # # Path 
+    # path = os.path.join(parent_dir, directory) 
+    
+ 
+    # os.mkdir(path) 
+
+    file = open(filename)
+    
+    reader = csv.reader(file)
+    batch_strength= len(list(reader))-1
+    print(batch_strength)
+
+    make_branch_strength(filename)
+
+
+
+
+    pass
+
+
+
+
+
+
+
+
+
+filename = "Btech_2020_master_data.csv"
+number_of_groups=input()
+
+group_allocation(filename, number_of_groups)
