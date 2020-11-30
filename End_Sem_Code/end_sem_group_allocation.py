77e4becb983f73ec58a2b8c22c3f7cf85864cdfa
mahathi2000 <mahathi.muddhey@gmail.com>
2020-11-30 07:34:03 +0530

    group folders created including the left roll numbers

--- End_Sem_Code/end_sem_group_allocation.py
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -69,10 +69,43 @@ def make_groups(filename,number_of_groups,batch_strength):
                 start=end
                 end=start+x
                 padded_group= str(group_num).zfill(2)
-                        
-                        
+
+    #code for the left over students
+    with open('branch_strength.csv','r') as file:
+        reader=csv.reader(file,delimiter=',')
+        next(reader)
+        p=0
+        for row in reader:
+
+            branch_strength=int(row[1])
+            #print(branch_strength)
+            branch_code=row[0]
+            x=math.floor(int(branch_strength)/int(number_of_groups))
+            left=branch_strength-(number_of_groups*x)
+            print(left)
+            start_index=branch_strength-left
+
+            csv_name=branch_code+'.csv'
+            df=pd.read_csv(csv_name)
+
+            for i in range(start_index,branch_strength):
+                group_number=p%12+1
+                padded_group= str(group_number).zfill(2)
+                group_file_name='Group_G'+padded_group+'.csv'
+                print(group_file_name)
+                with open(path+'/'+group_file_name,'a',newline='') as file:
+                    writer=csv.writer(file)
+                    row=list(df.iloc[i])
+                    writer.writerow(row)
+                    print(row)
+                print(p)
+                p=p+1
 
 
+
+                        
+
+    #make_stats_grouping(number_of_groups)
 
             
 
@@ -85,13 +118,59 @@ def make_groups(filename,number_of_groups,batch_strength):
             #         with open(path +'/'+ csv_name, 'a', newline='') as file:
             #         writer = csv.writer(file)
             #         writer.writerow(row)
-                
 
+    pass
+
+def make_stats_grouping(number_of_groups):
+    directory = "groups"
+    # Parent Directory path
+    parent_dir = os.getcwd()
+
+    # Path
+    path = os.path.join(parent_dir, directory)
+    stats_file_name='stats_grouping.csv'
+    header=['group','total','EE','ME','CS','CE','CB','MM']
+    with open(path +'/'+ stats_file_name, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+
+    for i in range(number_of_groups):
+        group_num=i+1
+        padded_group= str(group_num).zfill(2)
+        group_file_name='Group_G'+padded_group+'.csv'
+        print(group_file_name)
+        with open(group_file_name,'r') as file:
+            reader = csv.reader(file, delimiter=',')
+            lines=len(list(reader))
+        all_rolls=pd.read_csv(group_file_name)
+        branch_code=[]
+        with open(filename,'r') as file:
+            reader = csv.reader(file, delimiter=',')
+        
+            for row in reader:
+                roll=row[0]
+                branch=roll[4:6]
+                branch_code.append(branch)
+            all_rolls['BRANCH_CODE']=branch_code
+
+        #print(all_rolls)
+        df=all_rolls
+        new_df=df.groupby(['BRANCH_CODE']).size()
+        print(new_df)
+
+
+
+
+
+
+
+
+        
+        row=[group_file_name,lines,EE_count,ME_count,CS_count,CE_count,CB_count,MM_count]
+    pass
     
 
 
-
-    pass
 
 def make_individual_branch(filename):
     path=os.getcwd()
@@ -212,6 +291,7 @@ def group_allocation(filename, number_of_groups):
     
     #make_individual_branch(filename)
     make_groups(filename,number_of_groups,batch_strength)
+    #make_stats_grouping()
 
 
 
