d8f6fb306b8147c82ea058834429fd111f13cd25
mahathi2000 <mahathi.muddhey@gmail.com>
2020-12-03 20:40:13 +0530

    Added the Find and replace option in the edit menu

--- Projects/P2 Notepad_GUI/code_notepad_GUI.py
+++ Projects/P2 Notepad_GUI/code_notepad_GUI.py
@@ -111,6 +111,72 @@ def paste():
     pass
 
 
+def find_func(event=None):
+##using tag inbuilt function
+    def find():
+        word = find_input.get()
+        TextArea.tag_remove('match','1.0',tk.END)
+        matches = 0
+        if word :
+            start_pos = '1.0'
+            while True :
+                start_pos = TextArea.search(word,start_pos,stopindex=tk.END)
+                if(not start_pos):
+                    break
+                end_pos = f'{start_pos}+{len(word)}c'
+                TextArea.tag_add('match',start_pos,end_pos)
+                matches +=1
+                start_pos=end_pos
+                TextArea.tag_config('match',foreground='red',background='')
+
+
+
+
+    def replace():
+        word = find_input.get()
+        replace_text = replace_input.get()
+        content = TextArea.get(1.0,tk.END)
+        new_content = content.replace(word,replace_text)
+        TextArea.delete(1.0,tk.END)
+        TextArea.insert(1.0,new_content)
+
+
+    find_dialogue = tk.Toplevel()
+    find_dialogue.geometry('450x250+500+200')
+    find_dialogue.resizable(0,0)
+
+    ## frame
+    find_frame = ttk.LabelFrame(find_dialogue, text ='Find/Replace')
+    find_frame.pack(pady=20)
+
+    ## labels 
+    text_find_label = ttk.Label(find_frame,text ='Find :')
+    text_replace_label = ttk.Label(find_frame,text ='Replace')
+
+    ##entry boxes 
+    find_input = ttk.Entry(find_frame,width=30)
+    replace_input = ttk.Entry(find_frame,width=30)
+
+
+    ## Button
+    find_button = ttk.Button(find_frame,text ='Find',command=find)
+    replace_button = ttk.Button(find_frame,text ='Replace',command=replace)
+
+    ##label grid
+    text_find_label.grid(row=0,column=0,padx=4,pady=4)
+    text_replace_label.grid(row=1,column=0,padx=4,pady=4)
+
+    ##entry grid
+    find_input.grid(row=0, column=1,padx=4,pady=4)
+    replace_input.grid(row=1, column=1,padx=4,pady=4)
+
+    ##button grid
+    find_button.grid(row=2 ,column=0 ,padx=8,pady=4)
+    replace_button.grid(row=2 ,column=1 ,padx=8,pady=4)
+
+    find_dialogue.mainloop()
+
+
 
 # creating a menu bar
 
@@ -145,6 +211,7 @@ EditMenu= Menu(MenuBar,tearoff=0)
 EditMenu.add_command(label="Cut", command=cut)
 EditMenu.add_command(label="Copy", command=copy)
 EditMenu.add_command(label="Paste", command=paste)
+EditMenu.add_command(label="Find & Replace",command=find_func)
 
 MenuBar.add_cascade(label="Edit", menu=EditMenu)
 
